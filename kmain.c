d53ceca35450ee042b86ee9fce431526fd250fee
Sandunjayasekar <73893725+Sandunjayasekar@users.noreply.github.com>
2021-08-07 00:07:31 +0530

    Add files via upload (#3)

--- kmain.c
+++ kmain.c
@@ -1,4 +1,11 @@
-	int sum_of_three(int arg1, int arg2, int arg3)
-	{
-		return arg1 + arg2 + arg3;
-	}
+#include "frame_buffer.h"
+#include "serial_port.h"
+
+/* The C function */
+int sum_of_three(int a, int b, int c) {
+  char buffer[25] = "Welcome to SandunlinuxOS";
+  fb_write(buffer, 30);
+  serial_configure(SERIAL_COM1_BASE, Baud_115200);
+  serial_write(SERIAL_COM1_BASE, buffer, 26);
+  return a + b + c;
+}
